dfa2c4a9930b4c930d0ee3da02aaffdd74c8a55c
h181207 <h181207@stud.hib.no>
2017-05-13 21:07:46 +0200

    utleiekontor

--- Bilutleieselskap/src/bilutleie/Utleiekontor.java
+++ Bilutleieselskap/src/bilutleie/Utleiekontor.java
@@ -1,70 +1,51 @@
 package bilutleie;
 
 import java.util.*;
+import javax.persistence.*;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-/**
- * Utleiekontor-entitet.
- * 
- * @author Atle Geitung
- *
- */
 @Entity
-@Table(schema = "bilutleie")
+@Table(name = "utleiekontor", schema = "bilutleie")
 public class Utleiekontor {
     @Id
-    private Integer nummer;
-    private Integer telefon;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer kontorNR;
+    @Column(name = "tlf")
+    private Integer tlf;
+    
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "selskap", referencedColumnName = "selID")
+    private Selskap selskap;
 
     // Merk at dette er en aggregering, men IKKE komposisjon
     // Biler skal IKKE lagres når de legges til et kontor, de skal allerede være lagret
-    @OneToMany(mappedBy = "kontor")
+    @OneToMany(mappedBy = "utleiekontor")
     private List<Bil> biler;
 
     // Adresser skal lagres i databasen når de er laget og lagt til et kontor
     @OneToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "adresse", referencedColumnName = "id")
+    @JoinColumn(name = "adresse", referencedColumnName = "adrID")
     private Adresse adresse;
 
     @ManyToOne
-    @JoinColumn(name = "selskap", referencedColumnName = "navn", insertable = true, updatable = true)
-    private Bilutleieselskap selskap;
+    @JoinColumn(name = "selskap", referencedColumnName = "selID", insertable = true, updatable = true)
+    private Selskap selskap;    
     
-    @OneToMany
-    private List<Bestilling> bestillinger;
-    
-    
-    public Utleiekontor() {
-    	nummer = 0;
-    	telefon = 0;
-    	bestillinger = new ArrayList<>();
-    	biler = new ArrayList<>();
-    	
+    public Utleiekontor(){
+    	this(0,0,null,null,null);
     }
     
-    /**
-     * Leier ut bil 
-     * 
-     * @param bil
-     */
-    public void leieUt(Bil bil) {
+    public Utleiekontor(Integer kontorNR, Integer tlf, Selskap selskap, List<Bil> biler, Adresse adresse) {
+		this.kontorNR = kontorNR;
+		this.tlf = tlf;
+		this.selskap = selskap;
+		this.biler = biler;
+		this.adresse = adresse;
+	}
+    
+	public void leieUt(Bil bil) {
     	bil.setLedig(false);
     }
-    /**
-     * Tar en bil i retur
-     * 
-     * @param bil
-     */
+
     public void taImot(Bil bil) {
     	bil.setLedig(true);
     }
@@ -78,11 +59,7 @@ public class Utleiekontor {
     	}
     	return bil;
     }
-    /**
-     * Legger bil til biler.
-     * 
-     * @param bil
-     */
+
     public void leggTil(Bil bil) {
     	biler.add(bil);
     }
@@ -100,120 +77,54 @@ public class Utleiekontor {
     		System.out.println(biler.get(i).toString());
     	}
     }
+
+	public Integer getKontorNR() {
+		return kontorNR;
+	}
+
+	public void setKontorNR(Integer kontorNR) {
+		this.kontorNR = kontorNR;
+	}
+
+	public Integer getTlf() {
+		return tlf;
+	}
+
+	public void setTlf(Integer tlf) {
+		this.tlf = tlf;
+	}
+
+	public Selskap getSelskap() {
+		return selskap;
+	}
+
+	public void setSelskap(Selskap selskap) {
+		this.selskap = selskap;
+	}
+
+	public List<Bil> getBiler() {
+		return biler;
+	}
+
+	public void setBiler(List<Bil> biler) {
+		this.biler = biler;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public Selskap getSelskap() {
+		return selskap;
+	}
+
+	public void setSelskap(Selskap selskap) {
+		this.selskap = selskap;
+	}
     
-    /**
-     * Legger bestilling til bestillinger.
-     * 
-     * @param bestilling
-     */
-    public void leggTil(Bestilling bestilling) {
-    	bestillinger.add(bestilling);
-    }
-    /**
-     * Henter nummer.
-     *
-     * @return nummer
-     */
-    public Integer getNummer() {
-        return nummer;
-    }
-
-    /**
-     * Setter nummer.
-     *
-     * @param nummer nummer
-     */
-    public void setNummer(Integer nummer) {
-        this.nummer = nummer;
-    }
-
-    /**
-     * Henter telefon.
-     *
-     * @return telefon
-     */
-    public Integer getTelefon() {
-        return telefon;
-    }
-
-    /**
-     * Setter telefon.
-     *
-     * @param telefon telefon
-     */
-    public void setTelefon(Integer telefon) {
-        this.telefon = telefon;
-    }
-
-    /**
-     * Henter adresse.
-     *
-     * @return adresse
-     */
-    public Adresse getAdresse() {
-        return adresse;
-    }
-
-    /**
-     * Setter adresse.
-     *
-     * @param adresse adresse
-     */
-    public void setAdresse(Adresse adresse) {
-        this.adresse = adresse;
-    }
     
-    /**
-     * Henter biler.
-     * 
-     * @return
-     */
-    public List<Bil> getBiler() {
-    	return biler;
-    }
-    
-    /**
-     * Setter biler.
-     * 
-     * @param biler
-     */
-    public void setBiler(List<Bil> biler) {
-    	this.biler = biler;
-    }
-    
-    /**
-     * Henter selskap.
-     * 
-     * @return
-     */
-   public Bilutleieselskap getSelskap() {
-	   return selskap;
-   }
-   
-   /**
-    * Setter selskap.
-    * 
-    * @param selskap
-    */
-   public void setSelskap(Bilutleieselskap selskap) {
-	   this.selskap = selskap;
-   }
-   
-   /**
-    * Henter bestillinger.
-    * 
-    * @return
-    */
-   public List<Bestilling> getBestillinger() {
-	   return bestillinger;
-   }
-   
-   /**
-    * Setter bestillinger.
-    * 
-    * @param bestillinger
-    */
-   public void setBestillinger(List<Bestilling> bestillinger) {
-	   this.bestillinger = bestillinger;
-   }
 }
